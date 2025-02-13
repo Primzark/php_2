1dae5a0baec4afbb279fbebf29caf807d39b2ca1
Primzark <starlod7696@gmail.com>
2025-02-13 18:35:17 +0100

    chang

--- _index.php
+++ _index.php
@@ -20,13 +20,6 @@
 
 
 <?php
-$age= 22;
-
-if ($age >= 18) {
-    echo "Vous êtes un adulte"
-    } else {
-        echo "Vous êtes mineur"
-        }
 
        $magnitude = 8
 
---
+++ exo6.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<input type="checkbox" id="dark_mode" name="dark_mode">
+<label for="dark_mode">dark mode</label>
+
+<div id="myElement">
+<p>Hello World</p>
+</div>
+    
+</body>
+</html>
